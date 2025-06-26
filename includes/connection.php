40548cb9a524e1404fba7ca4f2c86bf3807c9cd5
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2025-06-27 00:33:28 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,10 +1,6 @@
 <?php
-$host = 'db'; // this matches the `service` name in docker-compose.yml
-$user = 'root';
-$pass = '';
-$db = 'bbms';
+$con = new mysqli("db", "root", "", "bbms");
 
-$con = new mysqli($host, $user, $pass, $db);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
