50096c00789250797358df564319cdc9248171a9
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2025-06-27 01:15:34 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("db", "root", "", "bbms");
+$con = new mysqli("dpg-d1ept3fdiees73b011h0-a", "bbms_db_r8om_user", "bbms_db_r8om_user","bbms", "5432");
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
