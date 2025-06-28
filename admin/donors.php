05c372d489c3219ea9234835d7ecdcaa63b57774
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2025-06-28 09:28:51 +0530

    Update donors.php

--- admin/donors.php
+++ admin/donors.php
@@ -1,7 +1,19 @@
+<?php
+session_start();
+include('../includes/connection.php');
+$query = "SELECT * FROM donors";
+$query_run = mysqli_query($connection, $query);
+$sno = 1;
+?>
+<!DOCTYPE html>
 <html>
-    <body>
-        <div class="row">
-            <div class="col-md-6 m-auto">
+<head>
+    <title>Donor List</title>
+    <!-- Add CSS, meta tags etc. here -->
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-6 m-auto">
             <br><center><h4><u>List of all Donors</u></h4><br></center>
             <table class="table">
                 <thead>
@@ -12,28 +24,28 @@
                     <th>Mobile No</th>
                     <th>Action</th>
                 </thead>
-                <?php 
-                    session_start();
-                    include('../includes/connection.php');
-                    $query = "select * from donors";
-                    $query_run = mysqli_query($connection,$query);
-                    $sno = 1;
-                    while($row = mysqli_fetch_assoc($query_run)){
-                        ?>
-                        <tr>
-                            <td><?php echo $sno; ?></td>
-                            <td><?php echo $row['id']; ?></td>
-                            <td><?php echo $row['name']; ?></td>
-                            <td><?php echo $row['email']; ?></td>
-                            <td><?php echo $row['mobile']; ?></td>
-                            <td><a class="btn btn-sm btn-success" href="edit_donor.php?did=<?php echo $row['id']; ?>">Edit</a> <a class="btn btn-sm btn-danger" href="delete_donor.php?did=<?php echo $row['id']; ?>">Delete</a></td>
-                        </tr>
-                        <?php
-                        $sno++;
-                    }
+                <tbody>
+                <?php
+                while ($row = mysqli_fetch_assoc($query_run)) {
+                    ?>
+                    <tr>
+                        <td><?php echo $sno; ?></td>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['email']; ?></td>
+                        <td><?php echo $row['mobile']; ?></td>
+                        <td>
+                            <a class="btn btn-sm btn-success" href="edit_donor.php?did=<?php echo $row['id']; ?>">Edit</a>
+                            <a class="btn btn-sm btn-danger" href="delete_donor.php?did=<?php echo $row['id']; ?>">Delete</a>
+                        </td>
+                    </tr>
+                    <?php
+                    $sno++;
+                }
                 ?>
+                </tbody>
             </table> 
-            </div>
-        </div>  
-    </body>
+        </div>
+    </div>  
+</body>
 </html>
