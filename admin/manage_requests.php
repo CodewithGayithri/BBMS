bbb376434cab79d7c44389ff7cbb82b9e44601e9
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2025-06-29 18:53:07 +0530

    Update manage_requests.php

--- admin/manage_requests.php
+++ admin/manage_requests.php
@@ -1,63 +1,82 @@
-<?php 
-session_start();
+<?php
+session_start(); 
+include_once '../includes/connection.php';
 ?>
+<!DOCTYPE html>
 <html>
+    <head>
+        <title>Manage Blood Requests</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
     <body>
         <div class="row">
             <div class="col-md-10 m-auto">
-            <br><center><h4><u>Manage Blood Requests</u></h4><br></center>
-            <table class="table">
-                <thead>
-                    <th>S.No</th>
-                    <th>Request ID</th>
-                    <th>Patient Name</th>
-                    <th>Mobile No</th>
-                    <th>Blood group</th>
-                    <th>Units(in ml)</th>
-                    <th>Reason</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </thead>
-                <?php 
-                    include('../includes/connection.php');
-                    $query = "select * from requests where status = 0";
-                    $query_run = mysqli_query($connection,$query);
-                    $sno = 1;
-                    while($row = mysqli_fetch_assoc($query_run)){
-                        $query1 = "select name,mobile from patients where id = $row[patient_id]";
-                        $query_run1 = mysqli_query($connection,$query1);
-                        while($row1 = mysqli_fetch_assoc($query_run1)){
-                            if($row['blood_group'] == 'A+'){
-                                $bg = 'AP';
-                            }elseif($row['blood_group'] == 'B+'){
-                                $bg = 'BP';
-                            }elseif($row['blood_group'] == 'AB+'){
-                                $bg = 'ABP';
-                            }elseif($row['blood_group'] == 'O+'){
-                                $bg = 'OP';
-                            }else{
-                                $bg = $row['blood_group'];
-                            }
-                        ?>
+                <br>
+                <center><h4><u>Manage Blood Requests</u></h4></center>
+                <br>
+                <table class="table table-striped table-bordered">
+                    <thead>
                         <tr>
-                            <td><?php echo $sno; ?></td>
-                            <td><?php echo $row['id']; ?></td>
-                            <td><?php echo $row1['name']; ?></td>
-                            <td><?php echo $row1['mobile']; ?></td>
-                            <td><?php echo $row['blood_group']; ?></td>
-                            <td><?php echo $row['no_units']; ?></td>
-                            <td><?php echo $row['reason']; ?></td>
-                            <td><?php if($row['status'] == 0){echo '<span class="badge bg-secondary">No Action</span>';} ?></td>
-                            <td><a class="btn btn-sm btn-success" href="accept_req.php?rid=<?php echo $row['id']; ?>&bg=<?php echo $bg; ?>&nu=<?php echo $row['no_units']; ?>">Approve</a>
-                            <a class="btn btn-sm btn-danger" href="reject_req.php?rid=<?php echo $row['id']; ?>">Reject</a></td>
+                            <th>S.No</th>
+                            <th>Request ID</th>
+                            <th>Patient Name</th>
+                            <th>Mobile No</th>
+                            <th>Blood Group</th>
+                            <th>Units (in ml)</th>
+                            <th>Reason</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
+                    </thead>
+                    <tbody>
                         <?php
+                        $query = "SELECT * FROM requests WHERE status = 0";
+                        $query_run = mysqli_query($connection, $query);
+                        $sno = 1;
+
+                        while ($row = mysqli_fetch_assoc($query_run)) {
+                            $stmt = $connection->prepare("SELECT name, mobile FROM patients WHERE id = ?");
+                            $stmt->bind_param("i", $row['patient_id']);
+                            $stmt->execute();
+                            $query_run1 = $stmt->get_result();
+
+                            while ($row1 = mysqli_fetch_assoc($query_run1)) {
+                                $bgMap = [
+                                    'A+' => 'AP',
+                                    'B+' => 'BP',
+                                    'AB+' => 'ABP',
+                                    'O+' => 'OP'
+                                ];
+                                $bg = $bgMap[$row['blood_group']] ?? $row['blood_group'];
+                                ?>
+                                <tr>
+                                    <td><?php echo htmlspecialchars($sno); ?></td>
+                                    <td><?php echo htmlspecialchars($row['id']); ?></td>
+                                    <td><?php echo htmlspecialchars($row1['name']); ?></td>
+                                    <td><?php echo htmlspecialchars($row1['mobile']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['blood_group']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['no_units']); ?></td>
+                                    <td><?php echo htmlspecialchars($row['reason']); ?></td>
+                                    <td>
+                                        <?php
+                                        if ($row['status'] == 0) {
+                                            echo '<span class="badge bg-secondary">No Action</span>';
+                                        }
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-sm btn-success" href="accept_req.php?rid=<?php echo urlencode($row['id']); ?>&bg=<?php echo urlencode($bg); ?>&nu=<?php echo urlencode($row['no_units']); ?>">Approve</a>
+                                        <a class="btn btn-sm btn-danger" href="reject_req.php?rid=<?php echo urlencode($row['id']); ?>">Reject</a>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                            $sno++;
                         }
-                        $sno++;
-                    }
-                ?>
-            </table> 
+                        ?>
+                    </tbody>
+                </table>
             </div>
-        </div>  
+        </div>
     </body>
 </html>
