f75b7849b987d582dbe20ac25eae9240581b29fa
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2025-06-29 10:59:54 +0530

    Update donors.php

--- admin/donors.php
+++ admin/donors.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['email'])) {
+    header('Location: ../login.php');
+    exit();
+}
 include('../includes/connection.php');
 $query = "SELECT * FROM donors";
 $query_run = mysqli_query($connection, $query);
@@ -9,43 +13,42 @@ $sno = 1;
 <html>
 <head>
     <title>Donor List</title>
-    <!-- Add CSS, meta tags etc. here -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="row">
-        <div class="col-md-6 m-auto">
-            <br><center><h4><u>List of all Donors</u></h4><br></center>
-            <table class="table">
-                <thead>
+    <div class="container mt-5">
+        <center><h4><u>List of all Donors</u></h4></center><br>
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
                     <th>S.No</th>
                     <th>Donor ID</th>
                     <th>Donor Name</th>
                     <th>Donor Email</th>
                     <th>Mobile No</th>
                     <th>Action</th>
-                </thead>
-                <tbody>
-                <?php
-                while ($row = mysqli_fetch_assoc($query_run)) {
-                    ?>
-                    <tr>
-                        <td><?php echo $sno; ?></td>
-                        <td><?php echo $row['id']; ?></td>
-                        <td><?php echo $row['name']; ?></td>
-                        <td><?php echo $row['email']; ?></td>
-                        <td><?php echo $row['mobile']; ?></td>
-                        <td>
-                            <a class="btn btn-sm btn-success" href="edit_donor.php?did=<?php echo $row['id']; ?>">Edit</a>
-                            <a class="btn btn-sm btn-danger" href="delete_donor.php?did=<?php echo $row['id']; ?>">Delete</a>
-                        </td>
-                    </tr>
-                    <?php
-                    $sno++;
-                }
-                ?>
-                </tbody>
-            </table> 
-        </div>
+                </tr>
+            </thead>
+            <tbody>
+            <?php while ($row = mysqli_fetch_assoc($query_run)) { ?>
+                <tr>
+                    <td><?php echo $sno; ?></td>
+                    <td><?php echo htmlspecialchars($row['id']); ?></td>
+                    <td><?php echo htmlspecialchars($row['name']); ?></td>
+                    <td><?php echo htmlspecialchars($row['email']); ?></td>
+                    <td><?php echo htmlspecialchars($row['mobile']); ?></td>
+                    <td>
+                        <a class="btn btn-sm btn-success" href="edit_donor.php?did=<?php echo $row['id']; ?>">Edit</a>
+                        <a class="btn btn-sm btn-danger" 
+                           href="delete_donor.php?did=<?php echo $row['id']; ?>" 
+                           onclick="return confirm('Are you sure you want to delete this donor?');">Delete</a>
+                    </td>
+                </tr>
+            <?php $sno++; } ?>
+            </tbody>
+        </table> 
     </div>  
 </body>
 </html>
