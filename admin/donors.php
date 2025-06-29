abfd939ecc510269d6a998b36200df2789602af5
Gayithri <167066150+CodewithGayithri@users.noreply.github.com>
2025-06-29 11:47:17 +0530

    Update donors.php

--- admin/donors.php
+++ admin/donors.php
@@ -1,46 +1,41 @@
-<?php
+<?php 
 session_start();
-include('../includes/connection.php');
-$query = "SELECT * FROM donors";
-$query_run = mysqli_query($connection, $query);
-$sno = 1;
 ?>
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>List of Donors</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    </head>
     <body>
-        <div class="container mt-5">
-            <center><h4><u>List of all Donors</u></h4></center><br>
-            <table class="table table-bordered table-striped">
-                <thead class="table-dark">
-                    <tr>
-                        <th>S.No</th>
-                        <th>Donor ID</th>
-                        <th>Donor Name</th>
-                        <th>Donor Email</th>
-                        <th>Mobile No</th>
-                        <th>Action</th>
-                    </tr>
+        <div class="row">
+            <div class="col-md-6 m-auto">
+            <br><center><h4><u>List of all Donors</u></h4><br></center>
+            <table class="table">
+                <thead>
+                    <th>S.No</th>
+                    <th>Donor ID</th>
+                    <th>Donor Name</th>
+                    <th>Donor Email</th>
+                    <th>Mobile No</th>
+                    <th>Action</th>
                 </thead>
-                <tbody>
-                    <?php while ($row = mysqli_fetch_assoc($query_run)) { ?>
+                <?php 
+                    include('../includes/connection.php');
+                    $query = "select * from donors";
+                    $query_run = mysqli_query($connection,$query);
+                    $sno = 1;
+                    while($row = mysqli_fetch_assoc($query_run)){
+                        ?>
                         <tr>
                             <td><?php echo $sno; ?></td>
-                            <td><?php echo htmlspecialchars($row['id']); ?></td>
-                            <td><?php echo htmlspecialchars($row['name']); ?></td>
-                            <td><?php echo htmlspecialchars($row['email']); ?></td>
-                            <td><?php echo htmlspecialchars($row['mobile']); ?></td>
-                            <td>
-                                <a class="btn btn-sm btn-success" href="edit_donor.php?did=<?php echo $row['id']; ?>">Edit</a>
-                                <a class="btn btn-sm btn-danger" href="delete_donor.php?did=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this donor?');">Delete</a>
-                            </td>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['email']; ?></td>
+                            <td><?php echo $row['mobile']; ?></td>
+                            <td><a class="btn btn-sm btn-success" href="edit_donor.php?did=<?php echo $row['id']; ?>">Edit</a> <a class="btn btn-sm btn-danger" href="delete_donor.php?did=<?php echo $row['id']; ?>">Delete</a></td>
                         </tr>
-                    <?php $sno++; } ?>
-                </tbody>
-            </table>
-        </div>
+                        <?php
+                        $sno++;
+                    }
+                ?>
+            </table> 
+            </div>
+        </div>  
     </body>
 </html>
